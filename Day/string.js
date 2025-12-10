f3fb496bc95b16e8ad86d32c0908c71dfe7d0a8a
Gulzar4054 <102143505+Gulzar4054@users.noreply.github.com>
2025-12-10 19:48:26 +0000

    Chnages on String

---
+++ Day/string.js
@@ -0,0 +1,38 @@
+const name = "Gulzar";
+const value = "$50";
+
+console.log(`Hello my name is ${name} and my Salary in Bussines is ${value}`)
+
+
+const Website = 'Guluzar.com';
+
+console.log(Website.length);
+console.log(Website.charAt(8));
+console.log(Website.charAt(7));
+console.log(Website.trim());
+
+const newString = Website.substring(0, 4)
+console.log(newString);
+console.log(Website.toUpperCase());
+console.log(Website.toLowerCase());
+
+
+const url = "https://www.Gulzar.com/learn/javascript/";
+
+console.log(url.includes("Gulzar.com"));
+console.log(url.includes("python"));
+console.log(url.startsWith("https"));
+console.log(url.endsWith("/"));
+
+const name2 = " Gulzar HUssain ";
+console.log(name2.trim());
+console.log(name2.replace("Gulzar HUssain", "Gulu YTZONE"));
+console.log(name2.replaceAll("G","g",));
+console.log(name2.replace(" ","_"));
+
+
+let arr = ["gulzar","huSsain","yTzone"];
+
+let newarr = arr.join("_");
+console.log(newarr);
+
