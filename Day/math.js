2207358286162904111b25c25a57daa41ba9a6da
Gulzar4054 <102143505+Gulzar4054@users.noreply.github.com>
2025-12-11 19:23:15 +0000

    Maths

---
+++ Day/math.js
@@ -0,0 +1,29 @@
+ const score = 400;
+
+ const balance = 99.99;
+
+ console.log(balance);
+ console.log(score);
+
+
+ const pi = 3.159;
+
+ console.log(Math.round(pi));
+ console.log(Math.ceil(pi));
+ console.log(Math.floor(pi));
+ 
+
+ const Dice = 10
+
+    console.log(Math.random(Dice)* 7 + 1);
+    console.log(Math.random(Dice)* 2 + 1);
+    console.log(Math.random(Dice)* 5 + 1);
+
+
+    console.log(Math.random()* 20);
+    
+
+
+    let min = 10
+    let max = 15
+    console.log(Math.floor(Math.random() * (max - min + 1)) + min);
