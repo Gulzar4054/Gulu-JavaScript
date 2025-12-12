41a13fd5b19825d3e9b68e8ea7bc4a130f6078db
Gulzar4054 <102143505+Gulzar4054@users.noreply.github.com>
2025-12-12 17:43:09 +0000

    Dates

---
+++ Day/dates.js
@@ -0,0 +1,16 @@
+let dates = new Date();
+
+console.log(dates.toString());
+
+let dates1 = new Date (2026,2,3);
+
+console.log(dates1.toLocaleString());
+console.log(dates1.getTime());
+console.log(dates1.getFullYear());
+console.log(dates1.getMonth());
+console.log(dates1.getDate());
+console.log(dates1.getDay()); 
+
+
+
+let dates3 = new Date()
